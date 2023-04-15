e6d213d638731c6ea45b1065ab0fdf213ea2caf3
nalayakofficial <130030313+nalayakofficial@users.noreply.github.com>
2023-04-15 08:26:19 +0545

    Update stanley.py

--- stanley.py
+++ stanley.py
@@ -245,8 +245,7 @@ def menu():
 def main():
     menu()
     while(True):
-        cnc = input('''\x1b[38;2;0;212;14m╔══[1337\x1b[38;2;0;186;45m@S\x1b[38;2;0;150;88mta\x1b[38;2;0;113;133mnl\x1b[38;2;0;83;168mey\x1b[38;2;0;49;147m]
-\x1b[38;2;0;212;14m╚\x1b[38;2;0;186;45m═\x1b[38;2;0;150;88m═\x1b[38;2;0;113;133m═\x1b[38;2;0;83;168m═\x1b[38;2;0;49;147m➤ \x1b[38;2;239;239;239m''')
+        cnc = input('''NMXST )-> ''')
         if cnc == "layer7" or cnc == "LAYER7" or cnc == "L7" or cnc == "l7":
             layer7()
         elif cnc == "layer4" or cnc == "LAYER4" or cnc == "L4" or cnc == "l4":
@@ -617,19 +616,6 @@ def main():
 
 
 def login():
-    clear()
-    user = "1337"
-    passwd = "1337"
-    username = input("</> Username: ")
-    password = getpass.getpass(prompt='</> Password: ')
-    if username != user or password != passwd:
-        print("")
-        print("</> Invalid credentials! Abandoning...")
-        sys.exit(1)
-    elif username == user and password == passwd:
-        print("</> Welcome to Stanley CnC!")
-        time.sleep(0.3)
-        ascii_vro()
         main()
 
 login()
